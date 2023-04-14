126a731f29ddd0d89edbff4eb873d7ac4e4c3dfc
Jake Creps <crepschrome@gmail.com>
2023-04-14 07:22:56 -0500

    Update script.js

--- script.js
+++ script.js
@@ -7,11 +7,12 @@ document.getElementById('email').addEventListener('keypress', async function (ev
 
         // Clear table content
         document.getElementById('result').innerHTML = '';
+        document.getElementById('image-container').innerHTML = '';
 
         // Show loading element
         document.getElementById('loading').classList.remove('hidden');
         // Hide table header
-        document.getElementById('result-table').querySelector('thead').classList.add('hidden');
+        document.getElementById('result-table').querySelector('thead').innerHTML = '';
 
         // Remove newsletter link if it exists
         if (newsletterLink) {
@@ -34,82 +35,165 @@ document.getElementById('email').addEventListener('keypress', async function (ev
 });
 
 function displayResults(data) {
-  const resultTable = document.getElementById('result');
-  resultTable.innerHTML = '';
+    const resultTable = document.getElementById('result');
+    resultTable.innerHTML = '';
 
-  for (const category in data) {
-    if (category === 'profiles') {
-      for (const platform in data[category]) {
-        const row = document.createElement('tr');
-        const platformCell = document.createElement('td');
-        platformCell.textContent = platform;
-        row.appendChild(platformCell);
+    let nameRowCreated = false;
 
-        const resultCell = document.createElement('td');
-        const icon = document.createElement('img');
-        icon.width = 16;
-        icon.height = 16;
+    // Create and insert the Name row at the top of the table
+    const nameRow = document.createElement('tr');
+    const namePlatformCell = document.createElement('td');
+    namePlatformCell.textContent = 'Name';
+    nameRow.appendChild(namePlatformCell);
+    const nameResultCell = document.createElement('td');
+    nameRow.appendChild(nameResultCell);
+    resultTable.insertBefore(nameRow, resultTable.firstChild);
 
-        if (data[category][platform] === 'true') {
-          icon.src = './img/true.png';
-        } else if (data[category][platform] === 'unknown') {
-          icon.src = './img/unknown.png';
+    for (const category in data) {
+        if (category === 'profiles') {
+            for (const platform in data[category]) {
+                const row = document.createElement('tr');
+                const platformCell = document.createElement('td');
+                platformCell.textContent = platform;
+                row.appendChild(platformCell);
+
+                const resultCell = document.createElement('td');
+                const iconWrapper = document.createElement('div');
+                const icon = document.createElement('img');
+                icon.width = 16;
+                icon.height = 16;
+
+                if (data[category][platform] === 'true') {
+                    icon.src = './img/true.png';
+                    iconWrapper.setAttribute('data-tooltip', 'Profile found');
+                } else if (data[category][platform] === 'unknown') {
+                    icon.src = './img/unknown.png';
+                    iconWrapper.setAttribute('data-tooltip', 'Profile status unknown');
+                } else {
+                    icon.src = './img/false.png';
+                    iconWrapper.setAttribute('data-tooltip', 'Profile not found');
+                }
+
+                iconWrapper.appendChild(icon);
+                resultCell.appendChild(iconWrapper);
+                row.appendChild(resultCell);
+
+                resultTable.appendChild(row);
+            }
         } else {
-          icon.src = './img/false.png';
+            if (category !== 'Image' && category !== 'Duolingo Name' && category !== 'GitHub Name') {
+                const row = document.createElement('tr');
+                const platformCell = document.createElement('td');
+                platformCell.textContent = category;
+                row.appendChild(platformCell);
+
+                const resultCell = document.createElement('td');
+                const iconWrapper = document.createElement('div');
+                const icon = document.createElement('img');
+                icon.width = 16;
+                icon.height = 16;
+
+                if (category === 'Location') {
+                    if (data[category] === 'N/A') {
+                        icon.src = './img/false.png';
+                        iconWrapper.setAttribute('data-tooltip', 'No location found');
+                        iconWrapper.appendChild(icon);
+                        resultCell.appendChild(iconWrapper);
+                    } else {
+                        resultCell.textContent = data[category];
+                    }
+                } else {
+                    if (data[category] === 'true') {
+                        icon.src = './img/true.png';
+                        if (category === 'Deliverable') {
+                            iconWrapper.setAttribute('data-tooltip', 'Email is deliverable');
+                        } else if (category === 'Disposable') {
+                            iconWrapper.setAttribute('data-tooltip', 'Email is disposable');
+                        } else if (category === 'Spam') {
+                            iconWrapper.setAttribute('data-tooltip', 'Email is spam');
+                        }
+                    } else {
+                        icon.src = './img/false.png';
+                        if (category === 'Deliverable') {
+                            iconWrapper.setAttribute('data-tooltip', 'Email is not deliverable');
+                        } else if (category === 'Disposable') {
+                            iconWrapper.setAttribute('data-tooltip', 'Email is not disposable');
+                        } else if (category === 'Spam') {
+                            iconWrapper.setAttribute('data-tooltip', 'Email is not spam');
+                        }
+                    }
+                    iconWrapper.appendChild(icon);
+                    resultCell.appendChild(iconWrapper);
+                }
+                row.appendChild(resultCell);
+                resultTable.appendChild(row);
+            }
         }
-
-        resultCell.appendChild(icon);
-        row.appendChild(resultCell);
-
-        resultTable.appendChild(row);
-      }
-    } else {
-      const row = document.createElement('tr');
-      const platformCell = document.createElement('td');
-      platformCell.textContent = category;
-      row.appendChild(platformCell);
-
-      const resultCell = document.createElement('td');
-      if (category === 'Name') {
-        if (data[category] === 'N/A') {
-          const icon = document.createElement('img');
-          icon.width = 16;
-          icon.height = 16;
-          icon.src = './img/false.png';
-          resultCell.appendChild(icon);
-        } else {
-          resultCell.textContent = data[category];
-        }
-      } else {
-        const icon = document.createElement('img');
-        icon.width = 16;
-        icon.height = 16;
-
-        if (data[category] === 'true') {
-          icon.src = './img/true.png';
-        } else if (data[category] === 'unknown') {
-          icon.src = './img/unknown.png';
-        } else {
-          icon.src = './img/false.png';
-        }
-
-        resultCell.appendChild(icon);
-      }
-
-      row.appendChild(resultCell);
-      resultTable.appendChild(row);
     }
-  }
 
-  // Show table header
-  document.getElementById('result-table').querySelector('thead').classList.remove('hidden');
+    let names = [];
 
-  // Append the newsletter link after the table if it doesn't exist
-  if (!newsletterLink) {
-    const tableContainer = document.getElementById('result-table');
-    newsletterLink = document.createElement('p');
-    newsletterLink.innerHTML = '<a href="https://osintnewsletter.com" target="_blank">Subscribe to The OSINT Newsletter</a>';
-    newsletterLink.id = 'newsletter-link';
-    tableContainer.parentNode.insertBefore(newsletterLink, tableContainer.nextSibling);
-  }
+    if (data['Duolingo Name'] !== 'N/A') {
+        names.push(data['Duolingo Name']);
+    }
+    if (data['GitHub Name'] !== 'N/A') {
+        names.push(data['GitHub Name']);
+    }
+
+    if (names.length > 0) {
+        nameResultCell.textContent = names.join(', ');
+    } else {
+        const iconWrapper = document.createElement('div');
+        const icon = document.createElement('img');
+        icon.width = 16;
+        icon.height = 16;
+        icon.src = './img/false.png';
+        iconWrapper.setAttribute('data-tooltip', 'No information found');
+        iconWrapper.appendChild(icon);
+        nameResultCell.appendChild(iconWrapper);
+    }
+
+    document.getElementById('result-table').querySelector('thead').classList.remove('hidden');
+
+    if (!newsletterLink) {
+        const tableContainer = document.getElementById('result-table');
+        newsletterLink = document.createElement('p');
+        newsletterLink.innerHTML = '<a href="https://osintnewsletter.com" target="_blank">Subscribe to The OSINT Newsletter</a>';
+        newsletterLink.id = 'newsletter-link';
+        tableContainer.parentNode.insertBefore(newsletterLink, tableContainer.nextSibling);
+    }
+
+    if (data.Image && data.Image !== 'false') {
+        const imageContainer = document.getElementById('image-container');
+        imageContainer.innerHTML = '';
+
+        if (data.Image) {
+            for (const platform in data.Image) {
+                // Add a check to ensure the image URL is not 'false' for Gravatar
+                if (data.Image[platform] !== 'false' && data.Image[platform] !== null) {
+                    let imageUrl = data.Image[platform];
+
+                    if (platform === 'Duolingo') {
+                        imageUrl = 'https:' + imageUrl + '/xlarge';
+                    } else if (platform === 'Gravatar') {
+                        imageUrl = imageUrl + '?s=400';
+                    }
+
+                    // Only display the image if the URL is not 'N/A'
+                    if (imageUrl !== 'N/A') {
+                        const img = document.createElement('img');
+                        img.src = imageUrl;
+                        img.alt = `${platform} profile image`;
+                        img.width = 100;
+                        img.height = 100;
+
+                        imageContainer.appendChild(img);
+                    }
+                }
+            }
+        }
+    } else {
+        // Clear image container if no image is found
+        document.getElementById('image-container').innerHTML = '';
+    }
 }
