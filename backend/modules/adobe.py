a7233abe00069902b5da99f4f3deb9e35c7fce03
Jake Creps <crepschrome@gmail.com>
2023-04-28 13:01:12 -0500

    Update adobe.py

--- backend/modules/adobe.py
+++ backend/modules/adobe.py
@@ -26,6 +26,7 @@ def adobe_email(email):
     else:
         return f'false'
 
+
 def adobe_facebook_email(email):
 
     headers = {
@@ -51,3 +52,35 @@ def adobe_facebook_email(email):
         return f'true'
     else:
         return f'unknown'
+
+
+def adobe_image(email):
+    # Make a request to retrieve the JSON data
+    headers = {
+        'content-type': 'application/json',
+        'x-ims-clientid': 'adobedotcom2',
+    }
+
+    json_data = {
+        'username': email,
+    }
+
+    response = requests.post(
+        'https://auth.services.adobe.com/signin/v2/users/accounts',
+        headers=headers,
+        json=json_data,
+    )
+
+    # Parse the JSON data
+    data = response.json()
+
+    # Get the image URL after "276"
+    try:
+        image_url = data[0]['images']['230']
+    except:
+        return f'N/A'
+
+    if image_url:
+        return image_url
+    else:
+        return f'N/A'
